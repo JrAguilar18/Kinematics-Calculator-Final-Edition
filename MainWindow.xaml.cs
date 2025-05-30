55b2f9854238f5c10694ffd7d905539132fd106e
Jiménez Garcia Carlos Abraham <charliepikachu06@gmail.com>
2025-05-30 00:49:40 -0700

    Kinematics ver 1.0.1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Kinematics_Calculator_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenDrawer_Click(object sender, RoutedEventArgs e)
+        {
+            MyDrawerHost.IsLeftDrawerOpen = true;
+        }
+
+        private void ToggleDrawer_Click(object sender, RoutedEventArgs e)
+        {
+            MyDrawerHost.IsLeftDrawerOpen = !MyDrawerHost.IsLeftDrawerOpen;
+        }
+
+        private void CloseDrawer_Click(object sender, RoutedEventArgs e)
+        {
+            MyDrawerHost.IsLeftDrawerOpen = false;
+        }
+    }
+}
